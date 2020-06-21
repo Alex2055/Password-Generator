a27995a8c556778cb965b854d0bcdcdaa32ea291
Alex2055 <alexfromnashville@gmail.com>
2020-06-21 09:32:29 -0500

    random array value

--- script.js
+++ script.js
@@ -14,28 +14,19 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //create an object to storage input data
-var passwordLength = "";
-var passwordType = [
-  {
-    lowercase: ""
-  },
-  {
-    uppercase: ""
-  },
-  {
-    numeric: ""
-  },
-  {
-    specialchar: ""
-  },
-
-]
+var passwordLength = 0;
+var passwordType = [];
+// var lowercase = "";
+// var uppercase = "";
+// var numeric = "";
+// var specialchar = "";
 
 //function to get the length of the password
 
 var lengthCheck = function () {
-  var length = window.prompt("Please choose the password length from 8 to 128 characters");
-
+  var lengthinput = window.prompt("Please choose the password length from 8 to 128 characters");
+  // convert string to int
+  var length = parseInt(lengthinput);
   // check if the password size is correct
   if (length < 8) {
     window.alert("The password is too short. Please try again");
@@ -45,13 +36,22 @@ var lengthCheck = function () {
     window.alert("The password is too long. Please try again");
     lengthCheck();
   }
+  if (length === "" && length === null) {
+    window.alert("Invalid input. Please try again");
+    lengthCheck();
+  }
+  // if (!isNaN(length)) {
+  //   window.alert("Length can only be a number");
+  //   lengthCheck();
+  // }
+
   else {
     passwordLength = length;
   }
 
   console.log(passwordLength)
+  console.log(typeof passwordLength)
 
-  typeCheck();
 }
 
 //function to get password types 
@@ -59,19 +59,22 @@ var lengthCheck = function () {
 var typeCheck = function () {
 
   var ucCheck = window.confirm("Would you like to have uppercase letters in your password?");
-  passwordType.lowercase = ucCheck;
+  passwordType[0] = ucCheck;
 
   var lcCheck = window.confirm("Would you like to have lowercase letters in your password?");
-  passwordType.lowercase = lcCheck;
+  passwordType[1] = lcCheck;
 
 
   var numCheck = window.confirm("Would you like to have numbers in your password?");
-  passwordType.lowercase = numCheck;
+  passwordType[2] = numCheck;
 
   var scCheck = window.confirm("Would you like to have special characters in your password?");
-  passwordType.lowercase = scCheck;
+  passwordType[3] = scCheck;
 
-  console.log(lcCheck, ucCheck, numCheck, scCheck);
+  console.log(passwordType[0]);
+  console.log(passwordType[1]);
+  console.log(passwordType[2]);
+  console.log(passwordType[3]);
 
   //check if at least one charachter type selected
 
@@ -79,41 +82,59 @@ var typeCheck = function () {
     window.alert("Please choose at least one type");
     typeCheck();
   }
-  genPass();
+
 }
 
-// generae password
+// generate password
 
 var genPass = function () {
 
-  var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
-  var lower = "qwertyuiopasdfghjklzxcvbnm";
-  var numb = "1234567890";
-  var schar = "!@#$%&*?><|}{+_-=`~;:";
-  var passcomb = "";
+  // random array value
+  var value = Math.floor(Math.random() * Math.floor(passwordType.length));
+  console.log(value);
+  console.log(typeof value);
 
-  //create the string with the combination of chosen characters 
 
-  if (passwordType.lowercase) {
-    passcomb = passcomb + upper;
-  }
-  if (passwordType.uppercase) {
-    passcomb = passcomb + lower;
-  }
-  if (passwordType.numeric) {
-    passcomb = passcomb + numb
-  }
-  if (passwordType.specialchar) {
-    passcomb = passcomb + schar;
-  }
-
-  console.log(passcomb);
 }
 
 
 
 lengthCheck();
+typeCheck();
+genPass();
 
+
+
+
+
+
+
+
+
+
+
+
+
+// var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  // var lower = "qwertyuiopasdfghjklzxcvbnm";
+  // var numb = "1234567890";
+  // var schar = "!@#$%&*?><|}{+_-=`~;:";
+  // var passcomb = "";
+
+  //create the string with the combination of chosen characters 
+
+  // if (passwordType[0]) {
+  //   passcomb = passcomb + upper;
+  // }
+  // if (passwordType[1]) {
+  //   passcomb = passcomb + lower;
+  // }
+  // if (passwordType[2]) {
+  //   passcomb = passcomb + numb
+  // }
+  // if (passwordType[3]) {
+  //   passcomb = passcomb + schar;
+  // }
 
 
 
